f965f27d1935c1a7066173d7db398547d5feba17
Chanthima Pinkaew <61321298+chunthm@users.noreply.github.com>
2020-04-06 20:25:29 +0700

    Update feature4-4.ino
    add 4-4 code
    but can't run on arduino

--- Assignment 4/4-4/feature4-4.ino
+++ Assignment 4/4-4/feature4-4.ino
@@ -1,6 +1,13 @@
 String examPassChack(int yourScore, int fullScore) {
   String result;
-
-
+    double full = fullScore/2;
+    if (yourScore >= full) {
+    result = "You are" + String(yourScore) + " point from " + String(fullScore) + 
+    " point in last exam.\nPass exam Congratulation.";
+    }
+    if (yourScore<full) {
+    result = "You are" + String(yourScore) + "point from " + String(fullScore) +
+    " point in last exam.\nFail exam. Attemp again";
+    }
   return result;
 }
