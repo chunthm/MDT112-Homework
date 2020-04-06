6ce51385c8ae73ff7253406c48e0d934f7ee34ff
Chanthima Pinkaew <61321298+chunthm@users.noreply.github.com>
2020-04-06 21:36:26 +0700

    Update feature4-5.ino
    add fxq 4-5

--- Assignment 4/4-5/feature4-5.ino
+++ Assignment 4/4-5/feature4-5.ino
@@ -1,5 +1,36 @@
 String gradeChack(int yourScore) {
   String result;
-
+    if (yourScore >= 85 && yourScore <= 100) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade A.";
+    }
+    if (yourScore >= 80 && yourScore <= 84) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade B+.";
+    }
+    if (yourScore >= 75 && yourScore <= 79) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade B.";
+    }
+    if (yourScore >= 70 && yourScore <= 74) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade C+.";
+    }
+    if (yourScore >= 65 && yourScore <= 69) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade C.";
+    }
+    if (yourScore >= 60 && yourScore <= 64) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade D+.";
+    }
+    if (yourScore >= 55 && yourScore <= 59) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade D.";
+    }
+    if (yourScore >= 75 && yourScore <= 79) {
+    result = "You are " + String(yourScore) + 
+    " point from Programming subject.\nYou get grade F.\n See you next semeject.";
+    }
   return result;
 }
